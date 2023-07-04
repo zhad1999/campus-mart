e5b5cb83b8071ab40a641d8b561c841a67ad50c2
zhad1999 <rizwan.zhad@woxsen.edu.in>
2023-07-04 14:24:33 +0530

    commit

---
+++ listings/choices.py
@@ -0,0 +1,54 @@
+category_choices = {
+  'Electronics': 'Electronics',
+  'Cars': 'Cars',
+  'Furniture': 'Furniture',
+  'Mobiles': 'Mobiles',
+  'Bikes': 'Bikes',
+  'Books&Sports': 'Books&Sports',
+  'Fashion': 'Fashion',
+  'other':'other'
+  }
+
+price_choices = {
+  '1000':'₹1,000',
+  '2000':'₹2,000',
+  '5000':'₹5,000',
+  '10000':'₹10,000',
+  '20000':'₹20,000',
+  '50000':'₹50,000',
+  '100000':'₹100,000',
+  '200000':'₹200,000',
+  '500000':'₹500,000',
+  '1000000':'₹1M+',
+  
+}
+
+state_choices = {
+  'AP': 'Andhra Pradesh',
+  'AR': 'Arunachal Pradesh',
+  'AS': 'Assam',
+  'BR': 'Bihar',
+  'CG': 'Chhattisgarh',
+  'GA': 'Goa',
+  'GJ': 'Gujarat',
+  'HP': 'Haryana',
+  'JH': 'Jharkhand',
+  'KA': 'Karnataka',
+  'KL': 'Kerala',
+  'MP': 'Madhya Pradesh',
+  'MH': 'Maharashtra',
+  'MN': 'Manipur',
+  'ML': 'Meghalaya',
+  'MZ': 'Mizoram',
+  'NL': 'Nagaland',
+  'OD': 'Odisha',
+  'PB': 'Punjab',
+  'RJ': 'Rajasthan',
+  'MI': 'Sikkim',
+  'TN': 'Tamil Nadu',
+  'TS': 'Telangana',
+  'TR': 'Tripura',
+  'UP': 'Uttar Pradesh',
+  'UK': 'Uttarakhand',
+  'WB': 'West Bengal',
+}
---
+++ listings/modelchoices.py
@@ -0,0 +1,41 @@
+category_choices = {
+  ('Electronics', 'Electronics'),
+  ('Cars', 'Cars'),
+  ('Furniture', 'Furniture'),
+  ('Mobiles', 'Mobiles'),
+  ('Bikes', 'Bikes'),
+  ('Books&Sports', 'Books&Sports'),
+  ('Fashion', 'Fashion'),
+  ('other', 'other')
+  }
+
+
+state_choices = {
+  ('AP', 'Andhra Pradesh'),
+  ('AR', 'Arunachal Pradesh'),
+  ('AS', 'Assam'),
+  ('BR', 'Bihar'),
+  ('CG', 'Chhattisgarh'),
+  ('GA', 'Goa'),
+  ('GJ', 'Gujarat'),
+  ('HP', 'Haryana'),
+  ('JH', 'Jharkhand'),
+  ('KA', 'Karnataka'),
+  ('KL', 'Kerala'),
+  ('MP', 'Madhya Pradesh'),
+  ('MH', 'Maharashtra'),
+  ('MN', 'Manipur'),
+  ('ML', 'Meghalaya'),
+  ('MZ', 'Mizoram'),
+  ('NL', 'Nagaland'),
+  ('OD', 'Odisha'),
+  ('PB', 'Punjab'),
+  ('RJ', 'Rajasthan'),
+  ('MI', 'Sikkim'),
+  ('TN', 'Tamil Nadu'),
+  ('TS', 'Telangana'),
+  ('TR', 'Tripura'),
+  ('UP', 'Uttar Pradesh'),
+  ('UK', 'Uttarakhand'),
+  ('WB', 'West Bengal'),
+}
